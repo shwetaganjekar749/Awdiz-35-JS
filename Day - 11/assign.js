89c616638f91a3b43267ddc9432bd0375b4af6a7
shwetaganjekar06-rgb <shwetaganjekar06@gmail.com>
2026-06-27 14:07:43 +0530

    Day - 11

---
+++ Day - 11/assign.js
@@ -0,0 +1,23 @@
+// Question 1 – Function with Parameters
+
+// Create a function named multiply that takes two numbers as parameters and returns their multiplication.
+// Example Output:
+// multiply(5, 4); // 20  
+
+function multiply(num1, num2){
+
+return num1 * num2;
+}
+
+console.log(5*4);
+
+// Question 2 – Function with Return
+// Create a function findSquare that accepts a number and returns its square.
+// Example Output:
+// findSquare(6); // 36
+
+function findSquare(num){
+    return num*num
+}
+
+console.log(findSquare(6));
